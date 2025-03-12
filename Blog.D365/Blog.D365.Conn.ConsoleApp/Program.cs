490dd551e8e5f18f0d8266404611a34b12bb0c13
Donghai Gong <donghai.gong@outlook.com>
2025-03-12 15:36:58 +0800

    Add：添加示例代码，创建App内的Notification

--- Blog.D365/Blog.D365.Conn.ConsoleApp/Program.cs
+++ Blog.D365/Blog.D365.Conn.ConsoleApp/Program.cs
@@ -19,12 +19,50 @@ namespace Blog.D365.Conn.ConsoleApp
                 // 使用 WhoAmI 进行测试
                 WhoAmIResponse resTest = (WhoAmIResponse)orgServiceorgService.Execute(new WhoAmIRequest());
                 Console.Write($"UserId: {resTest.UserId}");
+                //ExampleCreateAppNotification(orgServiceorgService);
                 Console.Read();
             }
             else
             {
                 throw new Exception(client.LastCrmError);
             }
+        }
+        private static void ExampleCreateAppNotification(IOrganizationService orgServiceorgService)
+        {
+            OrganizationRequest request = new OrganizationRequest()
+            {
+                RequestName = "SendAppNotification",
+                Parameters = new ParameterCollection
+                {
+                    ["Title"] = "(Form Console)Client assignment reminders",
+                    ["Recipient"] = new EntityReference("systemuser", new Guid("DDF2C431-A1DF-EE11-904D-0017FA06CFC8")),
+                    ["Body"] = "Customer [**Bright Design Studio**] has been assigned to you, please contact the customer in time.",
+                    ["IconType"] = new OptionSetValue(100000001),
+                    ["ToastType"] = new OptionSetValue(200000000),
+                    ["Actions"] = new Entity()
+                    {
+                        Attributes = {
+                            ["actions"] = new EntityCollection() {
+                                Entities = {
+                                    new Entity() {
+                                        Attributes = {
+                                            ["title"] = "Open Account record",
+                                            ["data"] = new Entity() {
+                                                Attributes = {
+                                                    ["type"] = "url",
+                                                    ["url"] = "?pagetype=entityrecord&etn=account&id=23956352-CEBA-EF11-B8E8-0017FA0527B1",
+                                                    ["navigationTarget"] = "newWindow"
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            orgServiceorgService.Execute(request);
         }
     }
 }
