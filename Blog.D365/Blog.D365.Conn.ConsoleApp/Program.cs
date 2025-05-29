571c1f813aa94b977a25eab66aa06d7d9d466aa6
Donghai Gong <donghai.gong@outlook.com>
2025-05-29 23:39:05 +0800

    Feat: 多选项取值

--- Blog.D365/Blog.D365.Conn.ConsoleApp/Program.cs
+++ Blog.D365/Blog.D365.Conn.ConsoleApp/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
 using Microsoft.Xrm.Tooling.Connector;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
+using Microsoft.Xrm.Sdk.Query;
 
 namespace Blog.D365.Conn.ConsoleApp
 {
@@ -14,20 +19,89 @@ namespace Blog.D365.Conn.ConsoleApp
             if (client.IsReady)
             {
                 IOrganizationService orgService = client;
-                NotificationService notificationService = new NotificationService(orgService);
-                notificationService.CreateAppNotification(
-                    "Client assignment reminders -- Form Console",
-                    new Guid("DDF2C431-A1DF-EE11-904D-0017FA06CFC8"),
-                    "Customer [**Bright Design Studio**] has been assigned to you, please contact the customer in time.",
-                    new OptionSetValue(100000001),
-                    new OptionSetValue(200000000),
-                    "?pagetype=entityrecord&etn=account&id=23956352-CEBA-EF11-B8E8-0017FA0527B1",
-                    "newWindow"
-                );
+                Entity cEntity = orgService.Retrieve("contact", Guid.Parse("EB5FE502-C9BA-EF11-B8E8-0017FA0527B1"),new ColumnSet("gdh_multi_select"));
+                SetMultiSelectText(orgService, cEntity);
+                //NotificationService notificationService = new NotificationService(orgService);
+                //notificationService.CreateAppNotification(
+                //    "Client assignment reminders -- Form Console",
+                //    new Guid("DDF2C431-A1DF-EE11-904D-0017FA06CFC8"),
+                //    "Customer [**Bright Design Studio**] has been assigned to you, please contact the customer in time.",
+                //    new OptionSetValue(100000001),
+                //    new OptionSetValue(200000000),
+                //    "?pagetype=entityrecord&etn=account&id=23956352-CEBA-EF11-B8E8-0017FA0527B1",
+                //    "newWindow"
+                //);
             }
             else
             {
                 throw new Exception(client.LastCrmError);
+            }
+        }
+        public static void SetMultiSelectText(IOrganizationService organization, Entity contactEn)
+        {
+            if (contactEn.Attributes.Contains("gdh_multi_select"))
+            {
+                OptionSetValueCollection optionSetValues =
+                    contactEn.GetAttributeValue<OptionSetValueCollection>("gdh_multi_select");
+                string roleText =
+                    GetMultiSelectOptionSetLabels(organization, "contact", "gdh_multi_select", optionSetValues);
+                if (!string.IsNullOrEmpty(roleText))
+                {
+                    Entity updateContact = new Entity(contactEn.LogicalName, contactEn.Id);
+                    updateContact["gdh_multi_select_text"] = roleText;
+                    organization.Update(updateContact);
+                }
+            }
+        }
+        public static string GetMultiSelectOptionSetLabels(
+            IOrganizationService service,
+            string entityLogicalName,
+            string attributeLogicalName,
+            OptionSetValueCollection values,
+            int? languageCode = null)
+        {
+            if (values == null || !values.Any())
+                return string.Empty;
+
+            // Retrieve the attribute metadata
+            RetrieveAttributeRequest retrieveAttributeRequest = new RetrieveAttributeRequest
+            {
+                EntityLogicalName = entityLogicalName,
+                LogicalName = attributeLogicalName,
+                RetrieveAsIfPublished = true
+            };
+
+            RetrieveAttributeResponse retrieveAttributeResponse = (RetrieveAttributeResponse)service.Execute(retrieveAttributeRequest);
+            MultiSelectPicklistAttributeMetadata attributeMetadata = retrieveAttributeResponse.AttributeMetadata as MultiSelectPicklistAttributeMetadata;
+
+            if (attributeMetadata == null)
+                throw new InvalidPluginExecutionException("Attribute is not a MultiSelectPicklistAttributeMetadata.");
+
+            // Prepare a map from option value to label
+            Dictionary<int, string> optionLabels = attributeMetadata.OptionSet.Options.ToDictionary(
+                o => o.Value.GetValueOrDefault(),
+                o => GetLocalizedLabel(o, languageCode)
+            );
+
+            // Map selected values to labels
+            var selectedLabels = values
+                .Select(v => optionLabels.ContainsKey(v.Value) ? optionLabels[v.Value] : $"(Unknown {v.Value})")
+                .ToList();
+
+            return string.Join(", ", selectedLabels);
+        }
+
+        private static string GetLocalizedLabel(OptionMetadata option, int? languageCode = null)
+        {
+            if (languageCode.HasValue)
+            {
+                var label = option.Label.LocalizedLabels
+                    .FirstOrDefault(l => l.LanguageCode == languageCode.Value);
+                return label?.Label ?? $"(No label for {option.Value})";
+            }
+            else
+            {
+                return option.Label.UserLocalizedLabel?.Label ?? $"(No label for {option.Value})";
             }
         }
     }
@@ -91,4 +165,5 @@ namespace Blog.D365.Conn.ConsoleApp
             _orgService.Execute(request);
         }
     }
+
 }
---
+++ Blog.D365/Blog.D365.Plugins/Contact/ContactPostUpdatePlugin.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Metadata;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blog.D365.Plugins.Contact
+{
+    public class ContactPostUpdatePlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context =
+                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            try
+            {
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                {
+                    IOrganizationServiceFactory factory =
+                        (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+                    IOrganizationService serviceAdmin = factory.CreateOrganizationService(null);
+                    Entity targetEntityRecord = (Entity)context.InputParameters["Target"];
+
+                    // "context.Stage == 40" -> PostOperation
+                    if (context.Stage == 40 && targetEntityRecord.Attributes.Contains("gdh_multi_select") &&
+                        (context.MessageName == "Create" || context.MessageName == "Update"))
+                    {
+                        SetMultiSelectText(serviceAdmin, targetEntityRecord);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                tracer.Trace($"ContactPostUpdatePlugin unexpected exception:\n{ex.Message}");
+                throw;
+            }
+        }
+        private void SetMultiSelectText(IOrganizationService organization, Entity contactEn)
+        {
+            if (contactEn.Attributes.Contains("gdh_multi_select"))
+            {
+                OptionSetValueCollection optionSetValues =
+                    contactEn.GetAttributeValue<OptionSetValueCollection>("gdh_multi_select");
+                string roleText =
+                    GetMultiSelectOptionSetLabels(organization, "contact", "gdh_multi_select", optionSetValues);
+                if (!string.IsNullOrEmpty(roleText))
+                {
+                    Entity updateContact = new Entity(contactEn.LogicalName, contactEn.Id);
+                    updateContact["gdh_multi_select_text"] = roleText;
+                    organization.Update(updateContact);
+                }
+            }
+        }
+        public static string GetMultiSelectOptionSetLabels(
+            IOrganizationService service,
+            string entityLogicalName,
+            string attributeLogicalName,
+            OptionSetValueCollection values,
+            int? languageCode = null)
+        {
+            if (values == null || !values.Any())
+                return string.Empty;
+
+            // Retrieve the attribute metadata
+            RetrieveAttributeRequest retrieveAttributeRequest = new RetrieveAttributeRequest
+            {
+                EntityLogicalName = entityLogicalName,
+                LogicalName = attributeLogicalName,
+                RetrieveAsIfPublished = true
+            };
+
+            RetrieveAttributeResponse retrieveAttributeResponse = (RetrieveAttributeResponse)service.Execute(retrieveAttributeRequest);
+            MultiSelectPicklistAttributeMetadata attributeMetadata = retrieveAttributeResponse.AttributeMetadata as MultiSelectPicklistAttributeMetadata;
+
+            if (attributeMetadata == null)
+                throw new InvalidPluginExecutionException("Attribute is not a MultiSelectPicklistAttributeMetadata.");
+
+            // Prepare a map from option value to label
+            Dictionary<int, string> optionLabels = attributeMetadata.OptionSet.Options.ToDictionary(
+                o => o.Value.GetValueOrDefault(),
+                o => GetLocalizedLabel(o, languageCode)
+            );
+
+            // Map selected values to labels
+            var selectedLabels = values
+                .Select(v => optionLabels.ContainsKey(v.Value) ? optionLabels[v.Value] : $"(Unknown {v.Value})")
+                .ToList();
+
+            return string.Join(", ", selectedLabels);
+        }
+
+        private static string GetLocalizedLabel(OptionMetadata option, int? languageCode = null)
+        {
+            if (languageCode.HasValue)
+            {
+                var label = option.Label.LocalizedLabels
+                    .FirstOrDefault(l => l.LanguageCode == languageCode.Value);
+                return label?.Label ?? $"(No label for {option.Value})";
+            }
+            else
+            {
+                return option.Label.UserLocalizedLabel?.Label ?? $"(No label for {option.Value})";
+            }
+        }
+    }
+}
